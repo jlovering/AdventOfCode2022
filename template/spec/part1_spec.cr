2b91dffa1aba6f2b6436554c40b7c168c6846922
Jon Lovering <jon.lovering@gmail.com>
2022-11-12 21:57:14 -0600

    Refactor to make nice

--- template/spec/part1_spec.cr
+++ template/spec/part1_spec.cr
@@ -1,18 +1,19 @@
 require "spec"
 require "../src/*"
 
+root = "./"
 day = "template"
 
 describe day do
     describe "Part1" do
         it "example test case" do
-            p1 = Part1.new("calendar/" + day + "/input/part1_example.txt")
+            p1 = Part1.new(root + "/" + day + "/input/example.txt")
             p1.run.should eq("")
         end
     end
     describe "Part1 solution" do
         pending "part1 input" do
-            p1 = Part1.new("calendar/" + day + "/input/part1.txt")
+            p1 = Part1.new(root + "/" + day + "/input/input.txt")
             result = p1.run
             Spec.after_suite { p! result}
         end
