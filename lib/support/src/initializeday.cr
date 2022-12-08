2bb8fe3824baa503042c48d57fcd63f75e07c654
Jon Lovering <jon.lovering@gmail.com>
2022-12-08 00:49:39 -0600

    Give the initializer some brains Add the work space correctly for the next AoC day (this doesn't work if run after midnight) Add an auto wait funciton for input grabbing.

--- lib/support/src/initializeday.cr
+++ lib/support/src/initializeday.cr
@@ -1,7 +1,33 @@
 require "./createday"
 require "./input_data"
+require "option_parser"
 
-raise ArgumentError.new() unless ARGV.size == 1
+day = 0
+wait = false
 
-CreateDay.new(ARGV[0].to_i).create_calendar
-InputData.new(ARGV[0].to_i).input
+OptionParser.parse do |parser|
+    parser.banner = "Usage: initializeday.cr [arguemtns]"
+    parser.on("-d DAY", "--day=DAY", "Specify the day") { |name| day = name.to_i}
+    parser.on("-w", "--wait", "Wait for Santa") { wait = true}
+    parser.on("-w", "--wait", "Wait for Santa") { wait = true}
+end
+
+if day == 0
+    day = Time.local.day
+end
+
+CreateDay.new(day+1).create_calendar
+
+if wait
+    now = Time.local
+    start = Time.local(now.year, now.month, day, 23, 0, 0)
+    time_to_start = start - now
+    if time_to_start > Time::Span.new(nanoseconds: 0)
+        puts "You're early sleeping for #{time_to_start}"
+        sleep(time_to_start)
+    else
+        puts "You're late hope to it!"
+    end
+end
+
+InputData.new(day+1).input
